17df0e602e540d26312a99c73c2e504249fff524
Luxum <coce@netcourrier.com>
2023-07-10 22:10:31 +0200

    create files and arrange workspace

---
+++ tc/CodeAnalysis/ExpressionSyntax.cs
@@ -0,0 +1,9 @@
+class ExpressionSyntax : SyntaxNode
+{
+    public override SyntaxKind Kind => throw new NotImplementedException();
+
+    public override IEnumerable<SyntaxNode> GetChildren()
+    {
+        throw new NotImplementedException();
+    }
+}
